d859fe9797da9a37388d962ca0bd3a4e1cb5bb47
Evgeniy <foksmix256@gmail.com>
2022-12-10 16:14:43 +0200

    fixed problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react"
-import { io, Socket } from 'socket.io-client'
-
+import { io, Socket } from "socket.io-client"
 
 const App = () => {
     const socketRef = React.useRef(null)
 
-
-
     React.useEffect(() => {
-        socketRef.current = io('https://socket-backend-test.vercel.app')
+        socketRef.current = io("https://socket-backend-test.vercel.app", {
+            transports: ["websocket", "polling"],
+        })
     }, [])
 
     return <div></div>
